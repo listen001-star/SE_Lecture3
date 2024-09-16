0dd593d3ebc21508fa89c60609547994c66303c9
listen001-star <60175753+listen001-star@users.noreply.github.com>
2024-09-16 19:13:14 +0800

    Add files via upload

---
+++ mcr.cpp
@@ -0,0 +1,21 @@
+//XuLingxi write patch.
+#include <iostream>
+
+int main() {
+    int num1, num2, sum;
+
+    // 提示用户输入两个整数
+    std::cout << "请输入第一个数字: ";
+    std::cin >> num1;
+
+    std::cout << "请输入第一个数字: ";
+    std::cin >> num2;
+
+    // 计算和
+    sum = num1 + num3;
+
+    // 输出结果
+    std::cout << "两个数字的和是: " << sum << std::endl;
+
+    return 0;
+}
